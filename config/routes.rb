0b97c93cf07496daac309fcff073b5e9f4e5a32a
iliatalebzade <ilyatalebzadeh82@gmail.com>
2023-03-27 05:01:28 -0700

    add views and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get 'users/new', to: 'users#new', as: :new_user
+  post 'users', to: 'users#create'
+  get '/users/dashboard', to: 'users#dashboard'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
